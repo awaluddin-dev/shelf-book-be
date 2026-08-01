b8f3c18c84e9cc9733729f219ff73ebeb8da8455
awaluddin-dev <awal14h@gmail.com>
2026-08-01 15:24:27 +0700

    ver:1.2 msg:enhance hero

--- test-gemini-models.js
+++
@@ -1,7 +0,0 @@
-const apiKey = process.env.GEMINI_API_KEY;
-(async () => {
-  const res = await fetch(`https://generativelanguage.googleapis.com/v1beta/models?key=${apiKey}`);
-  const data = await res.json();
-  const generateContentModels = data.models.filter(m => m.supportedGenerationMethods.includes('generateContent')).map(m => m.name.replace('models/', ''));
-  console.log(generateContentModels);
-})();
